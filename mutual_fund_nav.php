a9079981fcaeeb3959b3be3d6c57d9f858d22bac
Dharmesh Vasani <dharmeshkvasani@gmail.com>
2017-12-30 01:54:24 +0530

    nav prices

--- mutual_fund_nav.php
+++ mutual_fund_nav.php
@@ -9,7 +9,7 @@ $mutualFundArr = [
 ];
 $curl = curl_init();
 //Scheme Code;Scheme Name;Net Asset Value;Repurchase Price;Sale Price;Date
-curl_setopt_array($curl, array(
+curl_setopt_array($curl, [
     CURLOPT_URL => "http://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?frmdt=28-Dec-2017&todt=29-Dec-2017&tp=",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => "",
@@ -17,11 +17,8 @@ curl_setopt_array($curl, array(
     CURLOPT_TIMEOUT => 30,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_HTTPHEADER => array(
-        "cache-control: no-cache",
-        "postman-token: 5f7c0467-6eac-4ef9-154a-720b3c2e41cd",
-    ),
-));
+    CURLOPT_HTTPHEADER => [],
+]);
 
 $response = curl_exec($curl);
 $err = curl_error($curl);
@@ -42,13 +39,101 @@ if ($err) {
     }
 }
 ?>
-<html>
-<head> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous"></head>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+
+    <title>Mutual Funds NAV</title>
+  </head>
 <body>
-<div class="container">
+<div class="container-full">
 
 <div class="table-responsive">
-<h3 align="center">My Mutual Fund and ITs NAV Value</h3>
+<div class="page-header">
+    <h1 align="center">My Mutual Fund and ITs NAV Value</h1>
+</div>
+<div class ="row form-group">
+<div class="col-4"></div>
+<div class="col-2">
+      Select Mutual Fund
+</div>
+<div class="col-6">
+<select name="fund_name" id="fund_name" accesskey="M">
+			<option value="0">-- Select Mutual Fund --</option>
+			<option value="all">All</option>
+			<!-- <option value="39">ABN  AMRO Mutual Fund</option> -->
+			<option selected="selected" value="3">Aditya Birla Sun Life Mutual Fund</option>
+			<!-- <option value="50">AEGON Mutual Fund</option>
+			<option value="1">Alliance Capital Mutual Fund</option> -->
+			<option value="53">Axis Mutual Fund</option>
+			<!-- <option value="4">Baroda Pioneer Mutual Fund</option>
+			<option value="36">Benchmark Mutual Fund</option>
+			<option value="59">BNP Paribas Mutual Fund</option>
+			<option value="46">BOI AXA Mutual Fund</option>
+			<option value="32">Canara Robeco Mutual Fund</option>
+			<option value="60">Daiwa Mutual Fund</option>
+			<option value="31">DBS Chola Mutual Fund</option>
+			<option value="38">Deutsche Mutual Fund</option>
+			<option value="58">DHFL Pramerica Mutual Fund</option> -->
+			<option value="6">DSP BlackRock Mutual Fund</option>
+			<!-- <option value="47">Edelweiss Mutual Fund</option>
+			<option value="13">Escorts Mutual Fund</option>
+			<option value="54">Essel Mutual Fund</option>
+			<option value="40">Fidelity Mutual Fund</option>
+			<option value="51">Fortis Mutual Fund</option>
+			<option value="27">Franklin Templeton Mutual Fund</option>
+			<option value="8">GIC Mutual Fund</option>
+			<option value="49">Goldman Sachs Mutual Fund</option> -->
+			<option value="9">HDFC Mutual Fund</option>
+			<!-- <option value="37">HSBC Mutual Fund</option>
+			<option value="20">ICICI Prudential Mutual Fund</option>
+			<option value="57">IDBI Mutual Fund</option> -->
+			<option value="48">IDFC Mutual Fund</option>
+			<!-- <option value="68">IIFCL Mutual Fund (IDF)</option>
+			<option value="62">IIFL Mutual Fund</option> -->
+			<!-- <option value="11">IL&amp;F S Mutual Fund</option>
+			<option value="65">IL&amp;FS Mutual Fund (IDF)</option> -->
+			<!-- <option value="63">Indiabulls Mutual Fund</option>
+			<option value="14">ING Mutual Fund</option>
+			<option value="42">Invesco Mutual Fund</option>
+			<option value="16">JM Financial Mutual Fund</option>
+			<option value="43">JPMorgan Mutual Fund</option> -->
+			<option value="17">Kotak Mahindra Mutual Fund</option>
+			<option value="56">L&amp;T Mutual Fund</option>
+			<!-- <option value="18">LIC Mutual Fund</option>
+			<option value="69">Mahindra Mutual Fund</option>
+			<option value="45">Mirae Asset Mutual Fund</option>
+			<option value="19">Morgan Stanley Mutual Fund</option> -->
+			<option value="55">Motilal Oswal Mutual Fund</option>
+			<!-- <option value="44">PineBridge Mutual Fund</option>
+			<option value="34">PNB Mutual Fund</option>
+			<option value="64">PPFAS Mutual Fund</option>
+			<option value="10">PRINCIPAL Mutual Fund</option>
+			<option value="41">Quantum Mutual Fund</option> -->
+			<option value="21">Reliance Mutual Fund</option>
+			<!-- <option value="35">Sahara Mutual Fund</option> -->
+			<option value="22">SBI Mutual Fund</option>
+			<!-- <option value="52">Shinsei Mutual Fund</option>
+			<option value="67">Shriram Mutual Fund</option>
+			<option value="66">SREI Mutual Fund (IDF)</option>
+			<option value="2">Standard Chartered Mutual Fund</option>
+			<option value="24">SUN F&amp;C Mutual Fund</option>
+			<option value="33">Sundaram Mutual Fund</option> -->
+			<option value="25">Tata Mutual Fund</option>
+			<!-- <option value="26">Taurus Mutual Fund</option>
+			<option value="61">Union Mutual Fund</option>
+			<option value="28">UTI Mutual Fund</option>
+			<option value="29">Zurich India Mutual Fund</option> -->
+
+		</select>
+</div>
+</div>
 <table class="table">
   <thead class="thead-dark">
     <tr>
